b2357193d11d3c6dc69732b3b18ce8cbfa9b263c
Andrea Nodari <andrea.nodari91@gmail.com>
2012-11-22 08:26:32 +0100

    delete all

--- unitn_places/app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ActiveRecord::Base
-  attr_accessible :content, :title
-  belongs_to :place
-end
--- unitn_places/app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :email, :name, :username
-  has_and_belongs_to_many :places
-  acts_as_authentic
-end
